a5d63b4b2a37b0efda41c1c7b7f1cdce79091190
kennypal <kpalces@yahoo.com>
2017-09-13 02:48:07 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,35 @@
+function forLoop(array) {
+  for (let i = 0, i < 25; i++) {
+    if i = 1 {
+    console.log("I am 1 strange loop");
+  }
+    else {
+    console.log("I am ${i} strange loops");
+  }
+}
+  return array
+
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number)
+
+  }
+  return 'done'
+
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  array.pop
+} while (array.length > 0 && maybeTrue())
+
+}
+  return array
+
+}
